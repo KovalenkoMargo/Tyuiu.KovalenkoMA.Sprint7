f1abf5d4b03746a053e23f97f8249d7fa06bba8c
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-12-17 23:52:58 +0500

    Add project files.

---
+++ Tyuiu.KovalenkoMA.Sprint7.V2/FormMain_MA.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KovalenkoMA.Sprint7.V2
+{
+    public partial class FormMain_MA : Form
+    {
+        public FormMain_MA()
+        {
+            InitializeComponent();
+        }
+    }
+}
